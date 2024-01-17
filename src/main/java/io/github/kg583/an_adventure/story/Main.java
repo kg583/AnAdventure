ad81ec4948b28c252ad483880edd6d3b09feb926
KG <kgscience@hotmail.com>
2024-01-17 14:09:05 -0500

    I'm tired boss

--- src/main/java/io/github/kg583/an_adventure/story/Main.java
+++
@@ -1,9 +0,0 @@
-package io.github.kg583.an_adventure.story;
-
-import net.fabricmc.api.ModInitializer;
-
-public class Main implements ModInitializer {
-    @Override
-    public void onInitialize() {
-    }
-}
